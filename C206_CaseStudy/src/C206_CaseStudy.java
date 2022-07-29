857472dc25c3c1ff3f1471a1f1ed427e88c3bdde
Faye Ong <rpfaye@outlook.com>
2022-07-29 16:57:46 +0800

    Add list of items

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -5,12 +5,14 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Item> itemList = new ArrayList<Item>();
-
 		
-		System.out.println("Team 1 is the best");
-		
+		itemList.add(new Item(1, "Limited Edition RP Reusable Cup", "Condition: Brand New, Rose Gold Stainless Steel Cup with 500ml capacity.", 9.00, "01-02-2022","10-02-2022", 2.00));
+		itemList.add(new Item(2, "Limited Edition Stranger Things Tote Bag", "Condition: Like New, Bag is stranger than Stranger Things", 15.00, "01-02-2022", "11-02-2022", 5.00));
+		itemList.add(new Item(3, "Rainbow Care Bear Hat", "Condition: Brand New, we care about your head:)", 30.00, "01-02-2022", "09-02-2022", 10.00));
 		
 		
 	}
+	
+	
 
 }
